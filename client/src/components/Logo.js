c3d9e1c41c7eeb9a182b279d80822ff137c62371
Chirag <chiraganand10@outlook.com>
2025-04-22 16:50:16 +0530

    Initial commit for Career Connect

---
+++ client/src/components/Logo.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Wrapper from '../assets/wrappers/Logo';
+
+const Logo = () => {
+  return (
+    <Wrapper>
+        <div className='logo-container'>
+            <h1>C</h1>
+            <p>Career Connect</p>
+        </div>
+        
+    </Wrapper>
+  )
+}
+
+export default Logo
